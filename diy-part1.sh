13f59cb3cfeead3a1ba0603cd1e1cd8f93424cef
zongheng23333 <1954640276@qq.com>
2023-11-02 16:12:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,3 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-sed -i 's/l92.168.1.1/l92.168.6.1/g' package/base-files/files/bin/config_generate
-#修改默认主题为design
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
